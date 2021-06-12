264b166a5ae2d5420e3926e6026c1e08aac0ba29
Tristan Lei <tlei99@student.ubc.ca>
2021-06-12 02:02:34 -0700

    Finish up artist table with search, improved css

--- frontend/src/components/artist/index.js
+++ frontend/src/components/artist/index.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
+const ArtistRow = styled.tr`
+    padding: 10vh;
+    border: 1px solid black;
+`
+
 const ArtistWrapper = styled.td`
     text-align:center;
+    padding: 0.5vh;
 `
 
 const ArtistName = styled.h4`
@@ -10,11 +16,27 @@ const ArtistName = styled.h4`
   letter-spacing: 1px;
 `
 
+const DeleteButton = styled.div`
+    &:hover {
+        transition-duration: 0.45s;
+        background-color: gray;
+        text-decoration: none;
+        cursor: pointer;
+    }
+    cursor: pointer;
+    float: right;
+`
+
 function Artist(props){
     return(
-        <ArtistWrapper>
-        <ArtistName>{props.artistName}</ArtistName>
-        </ArtistWrapper>
+        <ArtistRow>
+            <ArtistWrapper>
+                <ArtistName>
+                    {props.artistName}
+                    <DeleteButton>x</DeleteButton>
+                </ArtistName>
+            </ArtistWrapper>
+        </ArtistRow>
     );
 }
 
