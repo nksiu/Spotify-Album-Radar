c08f8274169fad07ac51b562461e5fd0ddea3aa4
Kelvin Lo <kello@live.ca>
2021-06-12 00:04:19 -0700

    Add Navbar and route existing pages (#8)
    * add navbar and songs to router
    
    * remove yarn.lock from commited files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Components
-import NewReleases from './page/new-releases'
+import NewReleases from "./page/new-releases";
+import NavBar from "./components/navbar/NavBar";
+import Home from "./page/Home";
+import About from "./page/About";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Switch>
-          <Route exact path='/'>
-            <p>default route</p>
-          </Route>
-          <Route exact path='/songs'>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/songs">
             <NewReleases />
           </Route>
         </Switch>
---
+++ frontend/src/page/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <h1>About</h1>
+    </div>
+  );
+}
